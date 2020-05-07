77db5136e09518d604e6b378e01d2d6ed96a9065
José Guzmán <joseguzmanzamora@gmail.com>
2020-05-07 13:44:56 -0600

    Entendimiento q learning y deep q

--- test.py
+++ test.py
@@ -6,16 +6,19 @@ env = gym.make('KungFuMaster-v0')
 
 print(env.action_space)
 # Discrete 14 posibilidades
+actions = env.action_space.n
+
 print(env.observation_space)
 # una imagen de 210,160,3 shape
+dimensions = env.observation_space.shape
 
 
 env.reset()
-for _ in range(10000):
+for _ in range(1000):
     env.render()
-    save = env.step(env.action_space.contains("1"))
+    save = env.step(env.action_space.sample())
     image = save[0]
-    reward = save[1]
+    reward = save[1] 
     done = save[2]
     if done:
         # el personaje tiene 3 vidas para usar 
