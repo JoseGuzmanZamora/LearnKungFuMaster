02abf7290c62907fc96759403dcb37ef7f62b24e
José Guzmán <joseguzmanzamora@gmail.com>
2020-05-10 01:49:15 -0600

    comentarios claro como el agua

--- test.py
+++ test.py
@@ -11,16 +11,21 @@ actions = env.action_space.n
 print(env.observation_space)
 # una imagen de 210,160,3 shape
 dimensions = env.observation_space.shape
+r = 0
 
 
 env.reset()
-for _ in range(1000):
+step = 0
+for _ in range(10000):
     env.render()
     save = env.step(env.action_space.sample())
     image = save[0]
-    reward = save[1] 
+    r += save[1] 
     done = save[2]
     if done:
         # el personaje tiene 3 vidas para usar 
         break
+    step += 1
+
+print(r)
 env.close()
